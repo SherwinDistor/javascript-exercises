c8a2ef5c8ebc34a6062c8116e1b009058f75d870
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2024-11-25 11:53:42 -0600

    Reverse string using string index and .join method on array

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    const newStr = [];
 
+    for (let i = 0; i < str.length; i++) {
+        newStr.unshift(str[i]);
+    }
+
+    return newStr.join("");
 };
 
 // Do not edit below this line
