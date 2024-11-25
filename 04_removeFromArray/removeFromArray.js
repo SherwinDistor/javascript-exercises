d26575a45f4255222ca4302dcee30e7aec2b4b07
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2024-11-25 15:52:12 -0600

    Solve remove from array problem

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const newArray = array.filter(element => !args.includes(element));
+    return newArray;
 };
 
 // Do not edit below this line
