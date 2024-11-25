2430c3d66e1653ba9d8e943e97374aae26a668c9
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2024-11-25 15:52:26 -0600

    Solve sum all problem

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    } else if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    } else if (typeof(num1) !== "number" || typeof(num2) !== "number") {
+        return "ERROR";
+    } else {
+        let sum = 0;
+
+        if (num1 > num2) {
+            for (let i = num2; i <= num1; i++) {
+                sum += i;
+            }
+            return sum;
+        } else if (num2 > num1) {
+            for (let i = num1; i <= num2; i++) {
+                sum += i;
+            }
+            return sum;
+        } else {
+            sum = num1 + num2;
+            return sum;
+        }
+    }
 };
 
 // Do not edit below this line
