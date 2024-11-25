dcfb1cd560c2968535c38d8ee0981fc740d7a220
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2024-11-25 11:39:22 -0600

    Test if num < 0 then concat to repeat string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let finalString = "";
+    
+    if (num < 0) {
+        finalString = "ERROR";
+    } else {
+        for (let i = 1; i <= num; i++) {
+            finalString += string;
+        }
+    }
+    return finalString;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
