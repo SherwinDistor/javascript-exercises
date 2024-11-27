e935301f8f4d7bbbf47e4903e2aa88c0e82d962b
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2024-11-27 11:58:26 -0600

    Complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    
+    // Create a variable containing all valid characters
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    // Convert to lowercase, split to array of individual characters, filter only valid characters, then rejoin as a new string
+    const cleanedStr = str
+        .toLowerCase()
+        .split('')
+        .filter((char) => alphanumerical.includes(char))
+        .join('');
+
+    // Reverse the string to compare
+    const reversedStr = cleanedStr.split('').reverse().join('');
+
+    // return the boolean of the comparison
+    return cleanedStr === reversedStr;
 };
 
 // Do not edit below this line
